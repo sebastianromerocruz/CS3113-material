b2b9001ca38bbcb56bb89536a7ae29be15955e71
Thu Vu <tnv2002@nyu.edu>
2023-10-26 13:55:32 -0400

    Update Entity.h

--- lectures/sound-fx/SDLProject/Entity.h
+++ lectures/sound-fx/SDLProject/Entity.h
@@ -4,10 +4,10 @@ private:
     bool m_is_active = true;
 
     // ––––– ANIMATION ––––– //
-    int*  m_animation_right = NULL, // move to the right
-        * m_animation_left  = NULL, // move to the left
-        * m_animation_up    = NULL, // move upwards
-        * m_animation_down  = NULL; // move downwards
+    int* m_animation_right = NULL, // move to the right
+        * m_animation_left = NULL, // move to the left
+        * m_animation_up = NULL, // move upwards
+        * m_animation_down = NULL; // move downwards
 
     // ––––– PHYSICS (GRAVITY) ––––– //
     glm::vec3 m_position;
@@ -26,37 +26,37 @@ private:
 public:
     // ————— STATIC VARIABLES ————— //
     static const int SECONDS_PER_FRAME = 4;
-    static const int LEFT   = 0,
-                     RIGHT  = 1,
-                     UP     = 2,
-                     DOWN   = 3;
+    static const int LEFT = 0,
+        RIGHT = 1,
+        UP = 2,
+        DOWN = 3;
 
     // ————— ANIMATION ————— //
     int** m_walking = new int* [4]
         {
             m_animation_left,
-            m_animation_right,
-            m_animation_up,
-            m_animation_down
+                m_animation_right,
+                m_animation_up,
+                m_animation_down
         };
 
     int m_animation_frames = 0,
-        m_animation_index  = 0,
-        m_animation_cols   = 0,
-        m_animation_rows   = 0;
+        m_animation_index = 0,
+        m_animation_cols = 0,
+        m_animation_rows = 0;
 
     int* m_animation_indices = NULL;
-    float m_animation_time   = 0.0f;
+    float m_animation_time = 0.0f;
 
     // ––––– PHYSICS (JUMPING) ––––– //
-    bool  m_is_jumping     = false;
-    float m_jumping_power  = 0;
+    bool  m_is_jumping = false;
+    float m_jumping_power = 0;
 
     // ––––– PHYSICS (COLLISIONS) ––––– //
-    bool m_collided_top    = false;
+    bool m_collided_top = false;
     bool m_collided_bottom = false;
-    bool m_collided_left   = false;
-    bool m_collided_right  = false;
+    bool m_collided_left = false;
+    bool m_collided_right = false;
 
     GLuint    m_texture_id;
 
@@ -71,13 +71,13 @@ public:
 
     void update(float delta_time, Entity* collidable_entities, int collidable_entity_count);
     void render(ShaderProgram* program);
-    
-    void move_left()  { m_movement.x = -1.0f; };
-    void move_right() { m_movement.x = 1.0f;  };
-    void move_up()    { m_movement.y = 1.0f;  };
-    void move_down()  { m_movement.y = -1.0f; };
 
-    void activate()   { m_is_active = true; };
+    void move_left() { m_movement.x = -1.0f; };
+    void move_right() { m_movement.x = 1.0f; };
+    void move_up() { m_movement.y = 1.0f; };
+    void move_down() { m_movement.y = -1.0f; };
+
+    void activate() { m_is_active = true; };
     void deactivate() { m_is_active = false; };
 
     // ————— GETTERS ————— //
@@ -90,11 +90,11 @@ public:
     int       const get_height()       const { return m_height; };
 
     // ————— SETTERS ————— //
-    void const set_position(glm::vec3 new_position)         { m_position = new_position; };
-    void const set_velocity(glm::vec3 new_velocity)         { m_velocity = new_velocity; };
-    void const set_acceleration(glm::vec3 new_position)     { m_acceleration = new_position; };
-    void const set_movement(glm::vec3 new_movement)         { m_movement = new_movement; };
-    void const set_speed(float new_speed)                   { m_speed = new_speed; };
-    void const set_width(float new_width)                   { m_width = new_width; };
-    void const set_height(float new_height)                 { m_height = new_height; };
+    void const set_position(glm::vec3 new_position) { m_position = new_position; };
+    void const set_velocity(glm::vec3 new_velocity) { m_velocity = new_velocity; };
+    void const set_acceleration(glm::vec3 new_position) { m_acceleration = new_position; };
+    void const set_movement(glm::vec3 new_movement) { m_movement = new_movement; };
+    void const set_speed(float new_speed) { m_speed = new_speed; };
+    void const set_width(float new_width) { m_width = new_width; };
+    void const set_height(float new_height) { m_height = new_height; };
 };
