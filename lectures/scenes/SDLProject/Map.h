2b6bb3a9f45bc9e997af4483b3cf43a8cc025522
Thu Vu <tnv2002@nyu.edu>
2023-11-02 20:28:29 -0400

    Update Map.h

--- lectures/scenes/SDLProject/Map.h
+++ lectures/scenes/SDLProject/Map.h
@@ -18,42 +18,48 @@ private:
     int m_width;
     int m_height;
     
-    unsigned int *m_level_data;
-    GLuint        m_texture_id;
+    // Here, the level_data is the numerical "drawing" of the map
+    unsigned int    *m_level_data;
+    GLuint          m_texture_id;
     
     float m_tile_size;
     int   m_tile_count_x;
     int   m_tile_count_y;
     
+    // Just like with rendering text, we're rendering several sprites at once
+    // So we need vectors to store their respective vertices and texture coordinates
     std::vector<float> m_vertices;
     std::vector<float> m_texture_coordinates;
     
+    // The boundaries of the map
     float m_left_bound, m_right_bound, m_top_bound, m_bottom_bound;
     
 public:
+    // Constructor
     Map(int width, int height, unsigned int *level_data, GLuint texture_id, float tile_size, int
     tile_count_x, int tile_count_y);
     
+    // Methods
     void build();
     void render(ShaderProgram *program);
     bool is_solid(glm::vec3 position, float *penetration_x, float *penetration_y);
     
     // Getters
-    int const get_width()  const  { return this->m_width;  }
-    int const get_height() const  { return this->m_height; }
+    int const get_width()  const  { return m_width;  }
+    int const get_height() const  { return m_height; }
     
-    unsigned int* const get_level_data() const { return this->m_level_data; }
-    GLuint        const get_texture_id() const { return this->m_texture_id; }
+    unsigned int* const get_level_data() const { return m_level_data; }
+    GLuint        const get_texture_id() const { return m_texture_id; }
     
-    float const get_tile_size() const { return this->m_tile_size; }
-    int const get_tile_count_x() const { return this->m_tile_count_x; }
-    int const get_tile_count_y() const { return this->m_tile_count_y; }
+    float const get_tile_size()    const { return m_tile_size;    }
+    int   const get_tile_count_x() const { return m_tile_count_x; }
+    int   const get_tile_count_y() const { return m_tile_count_y; }
     
-    std::vector<float> const get_vertices()            const { return this->m_vertices;             }
-    std::vector<float> const get_texture_coordinates() const { return this-> m_texture_coordinates; }
+    std::vector<float> const get_vertices()            const { return m_vertices;             }
+    std::vector<float> const get_texture_coordinates() const { return m_texture_coordinates; }
     
-    float const get_left_bound()   const { return this->m_left_bound;   }
-    float const get_right_bound()  const { return this->m_right_bound;  }
-    float const get_top_bound()    const { return this->m_top_bound;    }
-    float const get_bottom_bound() const { return this->m_bottom_bound; }
+    float const get_left_bound()   const { return m_left_bound;   }
+    float const get_right_bound()  const { return m_right_bound;  }
+    float const get_top_bound()    const { return m_top_bound;    }
+    float const get_bottom_bound() const { return m_bottom_bound; }
 };
