af43e12d0efdda3663de7a07a75e67a9d40d0e23
Thu Vu <tnv2002@nyu.edu>
2023-11-09 20:33:23 -0500

    Update main.cpp

--- lectures/shaders/SDLProject/main.cpp
+++ lectures/shaders/SDLProject/main.cpp
@@ -27,32 +27,32 @@
 #include "Effects.h"
 
 // ––––– CONSTANTS ––––– //
-const int WINDOW_WIDTH  = 640,
-          WINDOW_HEIGHT = 480;
+const int WINDOW_WIDTH = 640,
+WINDOW_HEIGHT = 480;
 
-const float BG_RED     = 0.1922f,
-            BG_BLUE    = 0.549f,
-            BG_GREEN   = 0.9059f,
-            BG_OPACITY = 1.0f;
+const float BG_RED = 0.1922f,
+BG_BLUE = 0.549f,
+BG_GREEN = 0.9059f,
+BG_OPACITY = 1.0f;
 
 const int VIEWPORT_X = 0,
-          VIEWPORT_Y = 0,
-          VIEWPORT_WIDTH  = WINDOW_WIDTH,
-          VIEWPORT_HEIGHT = WINDOW_HEIGHT;
+VIEWPORT_Y = 0,
+VIEWPORT_WIDTH = WINDOW_WIDTH,
+VIEWPORT_HEIGHT = WINDOW_HEIGHT;
 
 const char V_SHADER_PATH[] = "shaders/vertex_lit.glsl",
-           F_SHADER_PATH[] = "shaders/fragment_lit.glsl";
+F_SHADER_PATH[] = "shaders/fragment_lit.glsl";
 
 const float MILLISECONDS_IN_SECOND = 1000.0;
 
 
 // ––––– GLOBAL VARIABLES ––––– //
-Scene  *g_current_scene;
-LevelA *g_levelA;
-LevelB *g_levelB;
+Scene* g_current_scene;
+LevelA* g_levelA;
+LevelB* g_levelB;
 
-Effects *g_effects;
-Scene   *g_levels[2];
+Effects* g_effects;
+Scene* g_levels[2];
 
 SDL_Window* g_display_window;
 bool g_game_is_running = true;
@@ -66,7 +66,7 @@ float g_accumulator = 0.0f;
 bool g_is_colliding_bottom = false;
 
 // ––––– GENERAL FUNCTIONS ––––– //
-void switch_to_scene(Scene *scene)
+void switch_to_scene(Scene* scene)
 {
     g_current_scene = scene;
     g_current_scene->initialise(); // DON'T FORGET THIS STEP!
@@ -76,44 +76,44 @@ void initialise()
 {
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
     g_display_window = SDL_CreateWindow("Hello, Special Effects!",
-                                      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                                      WINDOW_WIDTH, WINDOW_HEIGHT,
-                                      SDL_WINDOW_OPENGL);
-    
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_OPENGL);
+
     SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
     SDL_GL_MakeCurrent(g_display_window, context);
-    
+
 #ifdef _WINDOWS
     glewInit();
 #endif
-    
+
     glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
-    
+
     g_program.load(V_SHADER_PATH, F_SHADER_PATH);
-    
+
     g_view_matrix = glm::mat4(1.0f);
     g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
-    
+
     g_program.set_projection_matrix(g_projection_matrix);
     g_program.set_view_matrix(g_view_matrix);
-    
+
     glUseProgram(g_program.m_program_id);
-    
+
     glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
-    
+
     // enable blending
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     g_levelA = new LevelA();
     g_levelB = new LevelB();
-    
+
     g_levels[0] = g_levelA;
     g_levels[1] = g_levelB;
-    
+
     // Start at level A
     switch_to_scene(g_levels[0]);
-    
+
     g_effects = new Effects(g_projection_matrix, g_view_matrix);
     g_effects->start(SHRINK, 2.0f);
 }
@@ -122,43 +122,43 @@ void process_input()
 {
     // VERY IMPORTANT: If nothing is pressed, we don't want to go anywhere
     g_current_scene->m_state.player->set_movement(glm::vec3(0.0f));
-    
+
     SDL_Event event;
     while (SDL_PollEvent(&event))
     {
         switch (event.type) {
             // End game
-            case SDL_QUIT:
-            case SDL_WINDOWEVENT_CLOSE:
+        case SDL_QUIT:
+        case SDL_WINDOWEVENT_CLOSE:
+            g_game_is_running = false;
+            break;
+
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym) {
+            case SDLK_q:
+                // Quit the game with a keystroke
                 g_game_is_running = false;
                 break;
-                
-            case SDL_KEYDOWN:
-                switch (event.key.keysym.sym) {
-                    case SDLK_q:
-                        // Quit the game with a keystroke
-                        g_game_is_running = false;
-                        break;
-                        
-                    case SDLK_SPACE:
-                        // Jump
-                        if (g_current_scene->m_state.player->m_collided_bottom)
-                        {
-                            g_current_scene->m_state.player->m_is_jumping = true;
-                            Mix_PlayChannel(-1, g_current_scene->m_state.jump_sfx, 0);
-                        }
-                        break;
-                        
-                    default:
-                        break;
+
+            case SDLK_SPACE:
+                // Jump
+                if (g_current_scene->m_state.player->m_collided_bottom)
+                {
+                    g_current_scene->m_state.player->m_is_jumping = true;
+                    Mix_PlayChannel(-1, g_current_scene->m_state.jump_sfx, 0);
                 }
-                
+                break;
+
             default:
                 break;
+            }
+
+        default:
+            break;
         }
     }
-    
-    const Uint8 *key_state = SDL_GetKeyboardState(NULL);
+
+    const Uint8* key_state = SDL_GetKeyboardState(NULL);
 
     if (key_state[SDL_SCANCODE_LEFT])
     {
@@ -170,7 +170,7 @@ void process_input()
         g_current_scene->m_state.player->m_movement.x = 1.0f;
         g_current_scene->m_state.player->m_animation_indices = g_current_scene->m_state.player->m_walking[g_current_scene->m_state.player->RIGHT];
     }
-    
+
     if (glm::length(g_current_scene->m_state.player->m_movement) > 1.0f)
     {
         g_current_scene->m_state.player->m_movement = glm::normalize(g_current_scene->m_state.player->m_movement);
@@ -182,39 +182,40 @@ void update()
     float ticks = (float)SDL_GetTicks() / MILLISECONDS_IN_SECOND;
     float delta_time = ticks - g_previous_ticks;
     g_previous_ticks = ticks;
-    
+
     delta_time += g_accumulator;
-    
+
     if (delta_time < FIXED_TIMESTEP)
     {
         g_accumulator = delta_time;
         return;
     }
-    
+
     while (delta_time >= FIXED_TIMESTEP) {
         g_current_scene->update(FIXED_TIMESTEP);
         g_effects->update(FIXED_TIMESTEP);
-        
+
         if (g_is_colliding_bottom == false && g_current_scene->m_state.player->m_collided_bottom) g_effects->start(SHAKE, 1.0f);
-        
+
         g_is_colliding_bottom = g_current_scene->m_state.player->m_collided_bottom;
-        
+
         delta_time -= FIXED_TIMESTEP;
     }
-    
+
     g_accumulator = delta_time;
-    
+
     // Prevent the camera from showing anything outside of the "edge" of the level
     g_view_matrix = glm::mat4(1.0f);
-    
+
     if (g_current_scene->m_state.player->get_position().x > LEVEL1_LEFT_EDGE) {
         g_view_matrix = glm::translate(g_view_matrix, glm::vec3(-g_current_scene->m_state.player->get_position().x, 3.75, 0));
-    } else {
+    }
+    else {
         g_view_matrix = glm::translate(g_view_matrix, glm::vec3(-5, 3.75, 0));
     }
-    
+
     if (g_current_scene == g_levelA && g_current_scene->m_state.player->get_position().y < -10.0f) switch_to_scene(g_levelB);
-    
+
     g_view_matrix = glm::translate(g_view_matrix, g_effects->m_view_offset);
     g_program.set_light_position(g_current_scene->m_state.player->get_position());
 }
@@ -223,18 +224,18 @@ void render()
 {
     g_program.set_view_matrix(g_view_matrix);
     glClear(GL_COLOR_BUFFER_BIT);
- 
+
     glUseProgram(g_program.m_program_id);
     g_current_scene->render(&g_program);
     g_effects->render();
-    
+
     SDL_GL_SwapWindow(g_display_window);
 }
 
 void shutdown()
 {
     SDL_Quit();
-    
+
     delete g_levelA;
     delete g_levelB;
     delete g_effects;
@@ -244,17 +245,17 @@ void shutdown()
 int main(int argc, char* argv[])
 {
     initialise();
-    
+
     while (g_game_is_running)
     {
         process_input();
         update();
-        
+
         if (g_current_scene->m_state.next_scene_id >= 0) switch_to_scene(g_levels[g_current_scene->m_state.next_scene_id]);
-        
+
         render();
     }
-    
+
     shutdown();
     return 0;
 }
