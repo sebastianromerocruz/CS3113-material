84e6dfc83e4b2eb00475ad298968664517b6683f
Thu Vu <tnv2002@nyu.edu>
2023-11-09 20:31:30 -0500

    Update LevelA.cpp

--- lectures/shaders/SDLProject/LevelA.cpp
+++ lectures/shaders/SDLProject/LevelA.cpp
@@ -18,7 +18,7 @@ unsigned int LEVELA_DATA[] =
 
 LevelA::~LevelA()
 {
-    delete [] m_state.enemies;
+    delete[] m_state.enemies;
     delete    m_state.player;
     delete    m_state.map;
     Mix_FreeChunk(m_state.jump_sfx);
@@ -28,15 +28,15 @@ LevelA::~LevelA()
 void LevelA::initialise()
 {
     m_state.next_scene_id = -1;
-    
+
     GLuint map_texture_id = Utility::load_texture("assets/tileset.png");
     m_state.map = new Map(LEVEL_WIDTH, LEVEL_HEIGHT, LEVELA_DATA, map_texture_id, 1.0f, 4, 1);
-    
+
     // Code from main.cpp's initialise()
     /**
      George's Stuff
      */
-    // Existing
+     // Existing
     m_state.player = new Entity();
     m_state.player->set_entity_type(PLAYER);
     m_state.player->set_position(glm::vec3(5.0f, 0.0f, 0.0f));
@@ -44,29 +44,29 @@ void LevelA::initialise()
     m_state.player->m_speed = 2.5f;
     m_state.player->set_acceleration(glm::vec3(0.0f, -9.81f, 0.0f));
     m_state.player->m_texture_id = Utility::load_texture("assets/george_0.png");
-    
+
     // Walking
-    m_state.player->m_walking[m_state.player->LEFT]  = new int[4] { 1, 5, 9,  13 };
+    m_state.player->m_walking[m_state.player->LEFT] = new int[4] { 1, 5, 9, 13 };
     m_state.player->m_walking[m_state.player->RIGHT] = new int[4] { 3, 7, 11, 15 };
-    m_state.player->m_walking[m_state.player->UP]    = new int[4] { 2, 6, 10, 14 };
-    m_state.player->m_walking[m_state.player->DOWN]  = new int[4] { 0, 4, 8,  12 };
+    m_state.player->m_walking[m_state.player->UP] = new int[4] { 2, 6, 10, 14 };
+    m_state.player->m_walking[m_state.player->DOWN] = new int[4] { 0, 4, 8, 12 };
 
     m_state.player->m_animation_indices = m_state.player->m_walking[m_state.player->RIGHT];  // start George looking left
     m_state.player->m_animation_frames = 4;
-    m_state.player->m_animation_index  = 0;
-    m_state.player->m_animation_time   = 0.0f;
-    m_state.player->m_animation_cols   = 4;
-    m_state.player->m_animation_rows   = 4;
+    m_state.player->m_animation_index = 0;
+    m_state.player->m_animation_time = 0.0f;
+    m_state.player->m_animation_cols = 4;
+    m_state.player->m_animation_rows = 4;
     m_state.player->set_height(0.8f);
     m_state.player->set_width(0.8f);
-    
+
     // Jumping
     m_state.player->m_jumping_power = 5.0f;
-    
+
     /**
      Enemies' stuff */
     GLuint enemy_texture_id = Utility::load_texture("assets/soph.png");
-    
+
     m_state.enemies = new Entity[ENEMY_COUNT];
     m_state.enemies[0].set_entity_type(ENEMY);
     m_state.enemies[0].set_ai_type(GUARD);
@@ -76,28 +76,28 @@ void LevelA::initialise()
     m_state.enemies[0].set_movement(glm::vec3(0.0f));
     m_state.enemies[0].m_speed = 1.0f;
     m_state.enemies[0].set_acceleration(glm::vec3(0.0f, -9.81f, 0.0f));
-    
-    
+
+
     /**
      BGM and SFX
      */
     Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096);
-    
+
     m_state.bgm = Mix_LoadMUS("assets/dooblydoo.mp3");
     Mix_PlayMusic(m_state.bgm, -1);
     Mix_VolumeMusic(0.0f);
-    
+
     m_state.jump_sfx = Mix_LoadWAV("assets/bounce.wav");
 }
 
 void LevelA::update(float delta_time)
 {
     m_state.player->update(delta_time, m_state.player, m_state.enemies, ENEMY_COUNT, m_state.map);
-    
+
     if (m_state.player->get_position().y < -10.0f) m_state.next_scene_id = 1;
 }
 
-void LevelA::render(ShaderProgram *program)
+void LevelA::render(ShaderProgram* program)
 {
     m_state.map->render(program);
     m_state.player->render(program);
