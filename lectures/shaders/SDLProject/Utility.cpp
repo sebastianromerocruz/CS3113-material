719d4cdf6746f5917a3a789982b2876f4f55a2a5
Thu Vu <tnv2002@nyu.edu>
2023-11-09 20:33:05 -0500

    Update Utility.cpp

--- lectures/shaders/SDLProject/Utility.cpp
+++ lectures/shaders/SDLProject/Utility.cpp
@@ -13,34 +13,34 @@ GLuint Utility::load_texture(const char* filepath) {
     // STEP 1: Loading the image file
     int width, height, number_of_components;
     unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, STBI_rgb_alpha);
-    
+
     if (image == NULL)
     {
         LOG("Unable to load image. Make sure the path is correct.");
         assert(false);
     }
-    
+
     // STEP 2: Generating and binding a texture ID to our image
     GLuint texture_id;
     glGenTextures(NUMBER_OF_TEXTURES, &texture_id);
     glBindTexture(GL_TEXTURE_2D, texture_id);
     glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE, image);
-    
+
     // STEP 3: Setting our texture filter modes
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    
+
     // STEP 4: Setting our texture wrapping modes
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // the last argument can change depending on what you are looking for
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    
+
     // STEP 5: Releasing our file from memory and returning our texture id
     stbi_image_free(image);
-    
+
     return texture_id;
 }
 
-void Utility::draw_text(ShaderProgram *program, GLuint font_texture_id, std::string text, float screen_size, float spacing, glm::vec3 position)
+void Utility::draw_text(ShaderProgram* program, GLuint font_texture_id, std::string text, float screen_size, float spacing, glm::vec3 position)
 {
     // Scale the size of the fontbank in the UV-plane
     // We will use this for spacing and positioning
@@ -56,12 +56,12 @@ void Utility::draw_text(ShaderProgram *program, GLuint font_texture_id, std::str
     for (int i = 0; i < text.size(); i++) {
         // 1. Get their index in the spritesheet, as well as their offset (i.e. their position
         //    relative to the whole sentence)
-        int spritesheet_index = (int) text[i];  // ascii value of character
+        int spritesheet_index = (int)text[i];  // ascii value of character
         float offset = (screen_size + spacing) * i;
-        
+
         // 2. Using the spritesheet index, we can calculate our U- and V-coordinates
-        float u_coordinate = (float) (spritesheet_index % FONTBANK_SIZE) / FONTBANK_SIZE;
-        float v_coordinate = (float) (spritesheet_index / FONTBANK_SIZE) / FONTBANK_SIZE;
+        float u_coordinate = (float)(spritesheet_index % FONTBANK_SIZE) / FONTBANK_SIZE;
+        float v_coordinate = (float)(spritesheet_index / FONTBANK_SIZE) / FONTBANK_SIZE;
 
         // 3. Inset the current pair in both vectors
         vertices.insert(vertices.end(), {
@@ -71,7 +71,7 @@ void Utility::draw_text(ShaderProgram *program, GLuint font_texture_id, std::str
             offset + (0.5f * screen_size), -0.5f * screen_size,
             offset + (0.5f * screen_size), 0.5f * screen_size,
             offset + (-0.5f * screen_size), -0.5f * screen_size,
-        });
+            });
 
         texture_coordinates.insert(texture_coordinates.end(), {
             u_coordinate, v_coordinate,
@@ -80,25 +80,24 @@ void Utility::draw_text(ShaderProgram *program, GLuint font_texture_id, std::str
             u_coordinate + width, v_coordinate + height,
             u_coordinate + width, v_coordinate,
             u_coordinate, v_coordinate + height,
-        });
+            });
     }
 
     // 4. And render all of them using the pairs
     glm::mat4 model_matrix = glm::mat4(1.0f);
     model_matrix = glm::translate(model_matrix, position);
-    
+
     program->set_model_matrix(model_matrix);
     glUseProgram(program->m_program_id);
-    
+
     glVertexAttribPointer(program->m_position_attribute, 2, GL_FLOAT, false, 0, vertices.data());
     glEnableVertexAttribArray(program->m_position_attribute);
     glVertexAttribPointer(program->m_tex_coord_attribute, 2, GL_FLOAT, false, 0, texture_coordinates.data());
     glEnableVertexAttribArray(program->m_tex_coord_attribute);
-    
+
     glBindTexture(GL_TEXTURE_2D, font_texture_id);
-    glDrawArrays(GL_TRIANGLES, 0, (int) (text.size() * 6));
-    
+    glDrawArrays(GL_TRIANGLES, 0, (int)(text.size() * 6));
+
     glDisableVertexAttribArray(program->m_position_attribute);
     glDisableVertexAttribArray(program->m_tex_coord_attribute);
 }
-
