729148f307e8076153c014cbf7b5cb45c97376b5
Thu Vu <tnv2002@nyu.edu>
2023-11-09 20:33:13 -0500

    Update Utility.h

--- lectures/shaders/SDLProject/Utility.h
+++ lectures/shaders/SDLProject/Utility.h
@@ -16,5 +16,5 @@
 class Utility {
 public:
     static GLuint load_texture(const char* filepath);
-    static void draw_text(ShaderProgram *program, GLuint font_texture_id, std::string text, float screen_size, float spacing, glm::vec3 position);
+    static void draw_text(ShaderProgram* program, GLuint font_texture_id, std::string text, float screen_size, float spacing, glm::vec3 position);
 };
