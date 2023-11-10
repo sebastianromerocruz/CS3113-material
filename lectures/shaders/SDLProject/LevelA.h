1ce131b8ca18b8ff505ff43ebe2d927a8eefe4fa
Thu Vu <tnv2002@nyu.edu>
2023-11-09 20:31:39 -0500

    Update LevelA.h

--- lectures/shaders/SDLProject/LevelA.h
+++ lectures/shaders/SDLProject/LevelA.h
@@ -3,10 +3,10 @@
 class LevelA : public Scene {
 public:
     int ENEMY_COUNT = 1;
-    
+
     ~LevelA();
-    
+
     void initialise() override;
     void update(float delta_time) override;
-    void render(ShaderProgram *program) override;
+    void render(ShaderProgram* program) override;
 };
