baf7789618fef45f8a26a723819998c29af3480a
Thu Vu <tnv2002@nyu.edu>
2023-11-17 13:07:48 -0500

    Push fx exercise

---
+++ exercises/fx/SDLProject/LevelA.cpp
@@ -0,0 +1,104 @@
+#include "LevelA.h"
+#include "Utility.h"
+
+#define LEVEL_WIDTH 14
+#define LEVEL_HEIGHT 8
+
+unsigned int LEVELA_DATA[] =
+{
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+    3, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2,
+    3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2
+};
+
+LevelA::~LevelA()
+{
+    delete [] m_state.enemies;
+    delete    m_state.player;
+    delete    m_state.map;
+    Mix_FreeChunk(m_state.jump_sfx);
+    Mix_FreeMusic(m_state.bgm);
+}
+
+void LevelA::initialise()
+{
+    m_state.next_scene_id = -1;
+    
+    GLuint map_texture_id = Utility::load_texture("assets/tileset.png");
+    m_state.map = new Map(LEVEL_WIDTH, LEVEL_HEIGHT, LEVELA_DATA, map_texture_id, 1.0f, 4, 1);
+    
+    // Code from main.cpp's initialise()
+    /**
+     George's Stuff
+     */
+    // Existing
+    m_state.player = new Entity();
+    m_state.player->set_entity_type(PLAYER);
+    m_state.player->set_position(glm::vec3(5.0f, 0.0f, 0.0f));
+    m_state.player->set_movement(glm::vec3(0.0f));
+    m_state.player->set_speed(2.5f);
+    m_state.player->set_acceleration(glm::vec3(0.0f, -9.81f, 0.0f));
+    m_state.player->m_texture_id = Utility::load_texture("assets/george_0.png");
+    
+    // Walking
+    m_state.player->m_walking[m_state.player->LEFT]  = new int[4] { 1, 5, 9,  13 };
+    m_state.player->m_walking[m_state.player->RIGHT] = new int[4] { 3, 7, 11, 15 };
+    m_state.player->m_walking[m_state.player->UP]    = new int[4] { 2, 6, 10, 14 };
+    m_state.player->m_walking[m_state.player->DOWN]  = new int[4] { 0, 4, 8,  12 };
+
+    m_state.player->m_animation_indices = m_state.player->m_walking[m_state.player->RIGHT];  // start George looking left
+    m_state.player->m_animation_frames = 4;
+    m_state.player->m_animation_index  = 0;
+    m_state.player->m_animation_time   = 0.0f;
+    m_state.player->m_animation_cols   = 4;
+    m_state.player->m_animation_rows   = 4;
+    m_state.player->set_height(0.8f);
+    m_state.player->set_width(0.8f);
+    
+    // Jumping
+    m_state.player->m_jumping_power = 5.0f;
+    
+    /**
+     Enemies' stuff */
+    GLuint enemy_texture_id = Utility::load_texture("assets/soph.png");
+    
+    m_state.enemies = new Entity[ENEMY_COUNT];
+    m_state.enemies[0].set_entity_type(ENEMY);
+    m_state.enemies[0].set_ai_type(GUARD);
+    m_state.enemies[0].set_ai_state(IDLE);
+    m_state.enemies[0].m_texture_id = enemy_texture_id;
+    m_state.enemies[0].set_position(glm::vec3(8.0f, 0.0f, 0.0f));
+    m_state.enemies[0].set_movement(glm::vec3(0.0f));
+    m_state.enemies[0].set_speed(1.0f);
+    m_state.enemies[0].set_acceleration(glm::vec3(0.0f, -9.81f, 0.0f));
+    
+    
+    /**
+     BGM and SFX
+     */
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096);
+    
+    m_state.bgm = Mix_LoadMUS("assets/dooblydoo.mp3");
+    Mix_PlayMusic(m_state.bgm, -1);
+    Mix_VolumeMusic(0.0f);
+    
+    m_state.jump_sfx = Mix_LoadWAV("assets/bounce.wav");
+}
+
+void LevelA::update(float delta_time)
+{
+    m_state.player->update(delta_time, m_state.player, m_state.enemies, ENEMY_COUNT, m_state.map);
+    
+    if (m_state.player->get_position().y < -10.0f) m_state.next_scene_id = 1;
+}
+
+void LevelA::render(ShaderProgram *program)
+{
+    m_state.map->render(program);
+    m_state.player->render(program);
+}
---
+++ exercises/fx/solution/Effects.h
@@ -0,0 +1,42 @@
+#pragma once
+#define GL_SILENCE_DEPRECATION
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#define GL_GLEXT_PROTOTYPES 1
+#include <vector>
+#include <math.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+
+enum EffectType { NONE, FADEIN, FADEOUT, GROW, SHRINK, SHAKE }; // Added SHAKE
+
+class Effects {
+private:
+    ShaderProgram m_program;
+    EffectType    m_current_effect;
+    
+    float m_alpha;
+    float m_effect_speed;
+    float m_size;
+    float m_time_left;          // How long your effect will last
+
+
+public:
+    // How much your view matrix is shifted
+    glm::vec3 m_view_offset;
+    /** NOTE: We make this public so main update() can access the offset and
+     translate view_matrix accordingly */
+    
+    Effects(glm::mat4 projection_matrix, glm::mat4 view_matrix);
+
+    void draw_overlay();
+    void start(EffectType effect_type, float effect_speed);
+    void update(float delta_time);
+    void render();
+};
