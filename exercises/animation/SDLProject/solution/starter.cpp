c9034c36799f1c3aff49dae7c31ed2940e4cf912
Thu Vu <tnv2002@nyu.edu>
2024-02-29 21:51:21 -0500

    added unedited lectures and exercises for week6

---
+++ exercises/animation/SDLProject/solution/starter.cpp
@@ -0,0 +1,363 @@
+#define GL_SILENCE_DEPRECATION
+#define STB_IMAGE_IMPLEMENTATION
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#define GL_GLEXT_PROTOTYPES 1
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+#include "stb_image.h"
+
+enum Coordinate
+{
+    x_coordinate,
+    y_coordinate
+};
+
+#define LOG(argument) std::cout << argument << '\n'
+
+const int WINDOW_WIDTH = 640,
+          WINDOW_HEIGHT = 480;
+
+const float BG_RED = 0.9608f,
+            BG_BLUE = 0.9608f,
+            BG_GREEN = 0.9608f,
+            BG_OPACITY = 1.0f;
+
+const int VIEWPORT_X = 0,
+          VIEWPORT_Y = 0,
+          VIEWPORT_WIDTH = WINDOW_WIDTH,
+          VIEWPORT_HEIGHT = WINDOW_HEIGHT;
+
+const char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
+           F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
+
+const float MILLISECONDS_IN_SECOND = 1000.0;
+const float DEGREES_PER_SECOND     = 90.0f;
+
+const glm::vec3 ORIGIN = glm::vec3(0.0f, 0.0f, 0.0f),
+                DOUBLE = glm::vec3(2.0f, 2.0f, 0.0f);
+
+const int NUMBER_OF_TEXTURES = 1;
+const GLint LEVEL_OF_DETAIL  = 0;
+const GLint TEXTURE_BORDER   = 0;
+
+const char PLAYER_SPRITE_FILEPATH[] = "sprites/sonic.png",
+           FRAME_SPRITE_FILEPATH[]  = "sprites/frame.png",
+           FONT_SPRITE_FILEPATH[]   = "sprites/font1.png";
+
+const int CHARACTER_SHEET_ROWS = 1,
+          CHARACTER_SHEET_COLS = 4,
+          FRAME_ROWS           = 1,
+          FRAME_COLS           = 1;
+
+const int FONTBANK_SIZE        = 16,
+          FRAMES_PER_SECOND    = 4;
+
+SDL_Window* g_display_window;
+bool g_game_is_running = true;
+bool g_is_growing      = true;
+
+ShaderProgram g_character_program,
+              g_frame_program,
+              g_text_program;
+
+glm::mat4 g_view_matrix,
+          g_character_model_matrix,
+          g_frame_model_matrix,
+          g_projection_matrix;
+
+float g_previous_ticks = 0.0f;
+
+GLuint g_character_texture_id,
+       g_frame_texture_id,
+       g_text_texture_id;
+
+// ———— PART 1 ———— //
+
+// ———— PART 1 ———— //
+
+void draw_text(ShaderProgram *program, GLuint font_texture_id, std::string text, float screen_size, float spacing, glm::vec3 position)
+{
+    // Scale the size of the fontbank in the UV-plane
+    // We will use this for spacing and positioning
+    float width = 1.0f / FONTBANK_SIZE;
+    float height = 1.0f / FONTBANK_SIZE;
+
+    // Instead of having a single pair of arrays, we'll have a series of pairs—one for each character
+    // Don't forget to include <vector>!
+    std::vector<float> vertices;
+    std::vector<float> texture_coordinates;
+
+    // For every character...
+    for (int i = 0; i < text.size(); i++) {
+        // 1. Get their index in the spritesheet, as well as their offset (i.e. their position
+        //    relative to the whole sentence)
+        int spritesheet_index = (int) text[i];  // ascii value of character
+        float offset = (screen_size + spacing) * i;
+        
+        // 2. Using the spritesheet index, we can calculate our U- and V-coordinates
+        float u_coordinate = (float) (spritesheet_index % FONTBANK_SIZE) / FONTBANK_SIZE;
+        float v_coordinate = (float) (spritesheet_index / FONTBANK_SIZE) / FONTBANK_SIZE;
+
+        // 3. Inset the current pair in both vectors
+        vertices.insert(vertices.end(), {
+            offset + (-0.5f * screen_size), 0.5f * screen_size,
+            offset + (-0.5f * screen_size), -0.5f * screen_size,
+            offset + (0.5f * screen_size), 0.5f * screen_size,
+            offset + (0.5f * screen_size), -0.5f * screen_size,
+            offset + (0.5f * screen_size), 0.5f * screen_size,
+            offset + (-0.5f * screen_size), -0.5f * screen_size,
+        });
+
+        texture_coordinates.insert(texture_coordinates.end(), {
+            u_coordinate, v_coordinate,
+            u_coordinate, v_coordinate + height,
+            u_coordinate + width, v_coordinate,
+            u_coordinate + width, v_coordinate + height,
+            u_coordinate + width, v_coordinate,
+            u_coordinate, v_coordinate + height,
+        });
+    }
+
+    // 4. And render all of them using the pairs
+    glm::mat4 model_matrix = glm::mat4(1.0f);
+    model_matrix = glm::translate(model_matrix, position);
+    
+    program->set_model_matrix(model_matrix);
+    glUseProgram(program->get_program_id());
+    
+    glVertexAttribPointer(program->get_position_attribute(), 2, GL_FLOAT, false, 0, vertices.data());
+    glEnableVertexAttribArray(program->get_position_attribute());
+    glVertexAttribPointer(program->get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0, texture_coordinates.data());
+    glEnableVertexAttribArray(program->get_tex_coordinate_attribute());
+    
+    glBindTexture(GL_TEXTURE_2D, font_texture_id);
+    glDrawArrays(GL_TRIANGLES, 0, (int) (text.size() * 6));
+    
+    glDisableVertexAttribArray(program->get_position_attribute());
+    glDisableVertexAttribArray(program->get_tex_coordinate_attribute());
+}
+
+void draw_sprite_from_texture_atlas(ShaderProgram *program, GLuint texture_id, int index, int rows, int cols)
+{
+    // Step 1: Calculate the UV location of the indexed frame
+    float u_coord = (float) (index % cols) / (float) cols;
+    float v_coord = (float) (index / cols) / (float) rows;
+    
+    // Step 2: Calculate its UV size
+    float width = 1.0f / (float) cols;
+    float height = 1.0f / (float) rows;
+    
+    // Step 3: Match the texture coordinates to the vertices
+    float tex_coords[] =
+    {
+        u_coord, v_coord + height, u_coord + width, v_coord + height, u_coord + width, v_coord,
+        u_coord, v_coord + height, u_coord + width, v_coord, u_coord, v_coord
+    };
+    
+    float vertices[] =
+    {
+        -0.5, -0.5, 0.5, -0.5,  0.5, 0.5,
+        -0.5, -0.5, 0.5,  0.5, -0.5, 0.5
+    };
+
+    // Step 4: Render stuff
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+        
+    glVertexAttribPointer(program->get_position_attribute(), 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program->get_position_attribute());
+    
+    glVertexAttribPointer(program->get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0, tex_coords);
+    glEnableVertexAttribArray(program->get_tex_coordinate_attribute());
+    
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    
+    glDisableVertexAttribArray(program->get_position_attribute());
+    glDisableVertexAttribArray(program->get_tex_coordinate_attribute());
+}
+
+GLuint load_texture(const char* filepath)
+{
+    // STEP 1: Loading the image file
+    int width, height, number_of_components;
+    unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, STBI_rgb_alpha);
+    
+    if (image == NULL)
+    {
+        LOG("Unable to load image. Make sure the path is correct.");
+        assert(false);
+    }
+    
+    // STEP 2: Generating and binding a texture ID to our image
+    GLuint textureID;
+    glGenTextures(NUMBER_OF_TEXTURES, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE, image);
+    
+    // STEP 3: Setting our texture filter parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    
+    // STEP 4: Releasing our file from memory and returning our texture id
+    stbi_image_free(image);
+    
+    // Setting our texture wrapping modes
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // the last argument can change depending on what you are looking for
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    return textureID;
+}
+
+void initialise()
+{
+    // Initialise video and joystick subsystems
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK);
+    
+    g_display_window = SDL_CreateWindow("Hello, Animation!",
+                                      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                      WINDOW_WIDTH, WINDOW_HEIGHT,
+                                      SDL_WINDOW_OPENGL);
+    
+    SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
+    SDL_GL_MakeCurrent(g_display_window, context);
+    
+#ifdef _WINDOWS
+    glewInit();
+#endif
+    
+    glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+    
+    g_character_program.load(V_SHADER_PATH, F_SHADER_PATH);
+    g_frame_program.load(V_SHADER_PATH, F_SHADER_PATH);
+    g_text_program.load(V_SHADER_PATH, F_SHADER_PATH);
+    
+    // ————— INSTANTIATING VIEW AND PROJ MATRICES ———— //
+    g_view_matrix = glm::mat4(1.0f);  // Defines the position (location and orientation) of the camera
+    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);  // Defines the characteristics of your camera, such as clip planes, field of view, projection method etc.
+    
+    // ————— CHARACTER ———— //
+    g_character_model_matrix = glm::mat4(1.0f);
+    g_character_program.set_projection_matrix(g_projection_matrix);
+    g_character_program.set_view_matrix(g_view_matrix);
+    
+    glUseProgram(g_character_program.get_program_id());
+    g_character_texture_id = load_texture(PLAYER_SPRITE_FILEPATH);
+    
+    // ————— FRAME ———— //
+    g_frame_model_matrix     = glm::mat4(1.0f);
+    g_frame_program.set_projection_matrix(g_projection_matrix);
+    g_frame_program.set_view_matrix(g_view_matrix);
+    
+    glUseProgram(g_frame_program.get_program_id());
+    g_frame_texture_id = load_texture(FRAME_SPRITE_FILEPATH);
+    
+    // ———— TEXT ———— //
+    g_text_program.set_projection_matrix(g_projection_matrix);
+    g_text_program.set_view_matrix(g_view_matrix);
+    
+    glUseProgram(g_text_program.get_program_id());
+    g_text_texture_id = load_texture(FONT_SPRITE_FILEPATH);
+    
+    // ————— GENERAL ———— //
+    glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void process_input()
+{
+    SDL_Event event;
+    
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type) {
+            case SDL_WINDOWEVENT_CLOSE:
+            case SDL_QUIT:
+                g_game_is_running = false;
+                break;
+                
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym) {
+                    case SDLK_q:
+                        g_game_is_running = false;
+                        break;
+                    
+                    // ———— PART 2 ———— //
+
+                    // ———— PART 2 ———— //
+                        
+                    default:
+                        break;
+                }
+            default:
+                break;
+        }
+    }
+}
+
+void update()
+{
+    // ———— DELTA TIME CALCULATIONS ———— //
+    float ticks = (float) SDL_GetTicks() / MILLISECONDS_IN_SECOND; // get the current number of ticks
+    float delta_time = ticks - g_previous_ticks; // the delta time is the difference from the last frame
+    g_previous_ticks = ticks;
+    
+    // ———— PART 3 ———— //
+
+    // ———— PART 3 ———— //
+
+    // ———— RESETTING MODEL MATRICES ———— //
+    g_character_model_matrix = glm::mat4(1.0f);
+    g_frame_model_matrix     = glm::mat4(1.0f);
+    
+    g_character_model_matrix = glm::translate(g_character_model_matrix, ORIGIN);
+    g_frame_model_matrix     = glm::translate(g_frame_model_matrix, ORIGIN);
+    
+    g_frame_model_matrix     = glm::scale(g_frame_model_matrix, DOUBLE);
+    g_character_model_matrix = glm::scale(g_character_model_matrix, DOUBLE * 0.75f);
+}
+
+void render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    g_character_program.set_model_matrix(g_character_model_matrix);
+    
+    // ———— PART 4 ———— //
+    draw_sprite_from_texture_atlas(&g_character_program, g_character_texture_id, 0, CHARACTER_SHEET_ROWS, CHARACTER_SHEET_COLS);
+    // ———— PART 4 ———— //
+    
+    g_frame_program.set_model_matrix(g_frame_model_matrix);
+    draw_sprite_from_texture_atlas(&g_frame_program, g_frame_texture_id, 0, FRAME_ROWS, FRAME_COLS);
+    
+    draw_text(&g_text_program, g_text_texture_id, std::string("PRESS S TO"), 0.25f, 0.0f, glm::vec3(-1.25f, 2.0f, 0.0f));
+    draw_text(&g_text_program, g_text_texture_id, std::string("CHOOSE YOUR CHARACTER"), 0.25f, 0.01f, glm::vec3(-2.5f, 1.5f, 0.0f));
+    
+    SDL_GL_SwapWindow(g_display_window);
+}
+
+void shutdown() { SDL_Quit(); }
+
+/**
+ Start here—we can see the general structure of a game loop without worrying too much about the details yet.
+ */
+int main(int argc, char* argv[])
+{
+    initialise();
+    
+    while (g_game_is_running)
+    {
+        process_input();
+        update();
+        render();
+    }
+    
+    shutdown();
+    return 0;
+}
+
---
+++ lectures/physics_2/SDLProject/Entity.h
@@ -0,0 +1,100 @@
+class Entity
+{
+private:
+    bool m_is_active = true;
+
+    // ––––– ANIMATION ––––– //
+    int*  m_animation_right = NULL, // move to the right
+        * m_animation_left  = NULL, // move to the left
+        * m_animation_up    = NULL, // move upwards
+        * m_animation_down  = NULL; // move downwards
+
+    // ––––– PHYSICS (GRAVITY) ––––– //
+    glm::vec3 m_position;
+    glm::vec3 m_velocity;
+    glm::vec3 m_acceleration;
+
+    // ————— TRANSFORMATIONS ————— //
+    float     m_speed;
+    glm::vec3 m_movement;
+    glm::mat4 m_model_matrix;
+
+    float m_width = 1;
+    float m_height = 1;
+
+
+public:
+    // ————— STATIC VARIABLES ————— //
+    static const int SECONDS_PER_FRAME = 4;
+    static const int LEFT   = 0,
+                     RIGHT  = 1,
+                     UP     = 2,
+                     DOWN   = 3;
+
+    // ————— ANIMATION ————— //
+    int** m_walking = new int* [4]
+        {
+            m_animation_left,
+            m_animation_right,
+            m_animation_up,
+            m_animation_down
+        };
+
+    int m_animation_frames = 0,
+        m_animation_index  = 0,
+        m_animation_cols   = 0,
+        m_animation_rows   = 0;
+
+    int* m_animation_indices = NULL;
+    float m_animation_time   = 0.0f;
+
+    // ––––– PHYSICS (JUMPING) ––––– //
+    bool  m_is_jumping     = false;
+    float m_jumping_power  = 0;
+
+    // ––––– PHYSICS (COLLISIONS) ––––– //
+    bool m_collided_top    = false;
+    bool m_collided_bottom = false;
+    bool m_collided_left   = false;
+    bool m_collided_right  = false;
+
+    GLuint    m_texture_id;
+
+    // ————— METHODS ————— //
+    Entity();
+    ~Entity();
+
+    void draw_sprite_from_texture_atlas(ShaderProgram* program, GLuint texture_id, int index);
+    bool const check_collision(Entity* other) const;
+    void const check_collision_y(Entity* collidable_entities, int collidable_entity_count);
+    void const check_collision_x(Entity* collidable_entities, int collidable_entity_count);
+
+    void update(float delta_time, Entity* collidable_entities, int collidable_entity_count);
+    void render(ShaderProgram* program);
+    
+    void move_left()  { m_movement.x = -1.0f; };
+    void move_right() { m_movement.x = 1.0f;  };
+    void move_up()    { m_movement.y = 1.0f;  };
+    void move_down()  { m_movement.y = -1.0f; };
+
+    void activate()   { m_is_active = true; };
+    void deactivate() { m_is_active = false; };
+
+    // ————— GETTERS ————— //
+    glm::vec3 const get_position()     const { return m_position; };
+    glm::vec3 const get_velocity()     const { return m_velocity; };
+    glm::vec3 const get_acceleration() const { return m_acceleration; };
+    glm::vec3 const get_movement()     const { return m_movement; };
+    float     const get_speed()        const { return m_speed; };
+    int       const get_width()        const { return m_width; };
+    int       const get_height()       const { return m_height; };
+
+    // ————— SETTERS ————— //
+    void const set_position(glm::vec3 new_position)         { m_position = new_position; };
+    void const set_velocity(glm::vec3 new_velocity)         { m_velocity = new_velocity; };
+    void const set_acceleration(glm::vec3 new_position)     { m_acceleration = new_position; };
+    void const set_movement(glm::vec3 new_movement)         { m_movement = new_movement; };
+    void const set_speed(float new_speed)                   { m_speed = new_speed; };
+    void const set_width(float new_width)                   { m_width = new_width; };
+    void const set_height(float new_height)                 { m_height = new_height; };
+};
